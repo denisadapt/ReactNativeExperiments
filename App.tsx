84f99530f9849eb1e63f017c16f731debc0e3c1d
dk <hokum.gru@gmail.com>
2023-12-05 20:02:40 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,159 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  NativeModules,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import RNFS from 'react-native-fs';
+
+const {MyLibraryWrapper} = NativeModules;
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const showGreeting = async () => {
+    try {
+      const greeting = await MyLibraryWrapper.greet('World');
+      console.log(greeting);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const showAddition = async () => {
+    try {
+      const result = await MyLibraryWrapper.add(5, 3);
+      console.log(`Result: ${result}`);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const readLocalFile = async () => {
+    try {
+      console.log(RNFS.MainBundlePath);
+      const result = await MyLibraryWrapper.readFileContents(
+        RNFS.MainBundlePath + '/test.txt',
+      );
+      console.log(`Result: ${result}`);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <Section title="Greeting">
+            <Button title="Greet" onPress={showGreeting} />
+            <Button title="Add" onPress={showAddition} />
+            <Button title="Read file" onPress={readLocalFile} />
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ ios/MyLibraryWrapper.hh
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface MyLibraryWrapper : NSObject <RCTBridgeModule>
+@end
